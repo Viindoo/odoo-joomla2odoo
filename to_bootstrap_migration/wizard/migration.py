50c93fcf2dccfe498a16c96e36f8560a8a76679f
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-02-25 11:14:54 +0700

    to_bootstrap_migration: reorganize code for extensibility

--- to_bootstrap_migration/wizard/migration.py
+++ to_bootstrap_migration/wizard/migration.py
@@ -4,59 +4,16 @@ import re
 
 import lxml.html
 
-from odoo import fields, models, release
+from odoo import api, fields, models, release
 
 _logger = logging.getLogger(__name__)
-
-_bootstrap_replacement_rules = {
-    # 2 -> 3
-    2: [
-        (r'(?<!\S)row-fluid(?!\S)', 'row'),
-        (r'(?<!\S)span(?=([0-9]|10|11|12)(\s|$))', 'col-md-')
-    ],
-    # 3 -> 4
-    3: [
-        (r'(?<!\S)col-xs-(?=([0-9]|10|11|12)(\s|$))', 'col-'),
-        (r'(?<!\S)col-sm-(?=([0-9]|10|11|12)(\s|$))', 'col-md'),
-        (r'(?<!\S)col-md-(?=([0-9]|10|11|12)(\s|$))', 'col-lg'),
-        (r'(?<!\S)col-lg-(?=([0-9]|10|11|12)(\s|$))', 'col-xl'),
-        (r'(?<!\S)img-responsive(\s|$))', 'img-fluid')
-    ]
-}
-
-
-def _migrate_bootstrap(html, from_version, to_version):
-    from_version, to_version = int(from_version), int(to_version)
-
-    rules = []
-    for v in range(from_version, to_version):
-        rules.extend(_bootstrap_replacement_rules[v])
-
-    element = lxml.html.fromstring(html)
-    class_attr_elements = element.findall('.//*[@class]')
-    for el in class_attr_elements:
-        before = list(el.classes)
-
-        new_classes = []
-        for c in el.classes:
-            for rule in rules:
-                c = re.sub(*rule, c)
-            if c:
-                new_classes.append(c)
-        el.set('class', ' '.join(new_classes))
-
-        after = list(el.classes)
-        if after != before:
-            _logger.info('{} -> {}'.format(before, after))
-
-    return lxml.html.tostring(element, encoding='unicode')
 
 
 class BootstrapMigration(models.TransientModel):
     _name = 'bootstrap.migration'
     _description = 'Bootstrap Migration'
 
-    def _bootstrap_version(self):
+    def _current_version(self):
         odoo_version = release.version_info[0]
         if odoo_version == 12:
             return 4
@@ -65,22 +22,69 @@ class BootstrapMigration(models.TransientModel):
         return False
 
     def _supported_versions(self):
-        versions = [(2, '2'), (3, '3')]
-        to_version = self._bootstrap_version()
-        supported_versions = [v for v in versions if v[0] < to_version]
+        versions = [('2', '2'), ('2', '3')]
+        to_version = self._current_version()
+        supported_versions = [v for v in versions if int(v[0]) < to_version]
         return supported_versions
 
     from_version = fields.Selection(selection=_supported_versions, required=True)
-    to_version = fields.Integer(default=_bootstrap_version, readonly=True)
+    to_version = fields.Integer(default=_current_version, readonly=True)
 
     def run(self):
         self.ensure_one()
-        self._migrate_bootstrap_in_blog_posts()
+        self._migrate_blog_posts()
 
-    def _migrate_bootstrap_in_blog_posts(self):
+    @api.model
+    def _migrate_blog_posts(self):
         posts = self.env['blog.post'].search([])
         for post in posts:
             _logger.info('migrating bootstrap in blog post {}'.format(post.name))
-            new_content = _migrate_bootstrap(post.content, self.from_version,
-                                             self.to_version)
+            new_content = self._migrate_content(post.content, self.from_version,
+                                                self.to_version)
             post.content = new_content
+
+    @api.model
+    def _migration_rules(self):
+        return {
+            # 2 -> 3
+            2: [
+                (r'(?<!\S)row-fluid(?!\S)', 'row'),
+                (r'(?<!\S)span(?=([0-9]|10|11|12)(\s|$))', 'col-md-')
+            ],
+            # 3 -> 4
+            3: [
+                (r'(?<!\S)col-xs-(?=([0-9]|10|11|12)(\s|$))', 'col-'),
+                (r'(?<!\S)col-sm-(?=([0-9]|10|11|12)(\s|$))', 'col-md'),
+                (r'(?<!\S)col-md-(?=([0-9]|10|11|12)(\s|$))', 'col-lg'),
+                (r'(?<!\S)col-lg-(?=([0-9]|10|11|12)(\s|$))', 'col-xl'),
+                (r'(?<!\S)img-responsive(\s|$))', 'img-fluid')
+            ]
+        }
+
+    @api.model
+    def _migrate_content(self, html, from_version, to_version, output='html'):
+        from_version, to_version = int(from_version), int(to_version)
+
+        all_rules = self._migration_rules()
+        rules = []
+        for v in range(from_version, to_version):
+            rules.extend(all_rules[v])
+
+        element = lxml.html.fromstring(html)
+        class_attr_elements = element.findall('.//*[@class]')
+        for el in class_attr_elements:
+            before = list(el.classes)
+
+            new_classes = []
+            for c in el.classes:
+                for rule in rules:
+                    c = re.sub(*rule, c)
+                if c:
+                    new_classes.append(c)
+            el.set('class', ' '.join(new_classes))
+
+            after = list(el.classes)
+            if after != before:
+                _logger.info('{} -> {}'.format(before, after))
+
+        return lxml.html.tostring(element, encoding='unicode', method=output)
