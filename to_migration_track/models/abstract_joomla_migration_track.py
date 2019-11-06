42a169da3871d15e69ffe93eaa482e0c7c5d0cd3
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-11-06 17:02:41 +0700

    [FIX] *: fix warning

--- to_migration_track/models/abstract_joomla_migration_track.py
+++ to_migration_track/models/abstract_joomla_migration_track.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 class AbstractJoomlaMigrationTrack(models.AbstractModel):
     _name = 'abstract.joomla.migration.track'
+    _description = 'Abstract Joomla Migration Track'
 
     old_website = fields.Char()
     old_website_model = fields.Char()
