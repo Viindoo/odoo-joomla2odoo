7ec64a54403551308edddbff19650e5889114c04
Trinh Anh Ngoc <ngoc.trinh@tvtmarine.com>
2019-12-27 13:48:00 +0700

    [ADD] to_erponline_modules_intro_page: new module for modules page

---
+++ to_erponline_modules_intro_page/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "ERPOnline Modules Intro Page",
+    'summary': """Modules intro page on ERPOnline website""",
+    'description': """""",
+    'author': "T.V.T Marine Automation (aka TVTMA)",
+    'website': 'https://ma.tvtmarine.com',
+    'support': 'support@ma.tvtmarine.com',
+    'category': 'Extra Tools',
+    'version': '0.1',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'views/templates.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 0.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+}
